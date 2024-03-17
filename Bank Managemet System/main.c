55048804f36113077e7e1ef326e225f85645ed4a
Vysakh <vysakhcs7@gmail.com>
2024-03-17 20:48:16 +0530

    New update

--- Bank Managemet System/main.c
+++ Bank Managemet System/main.c
@@ -9,7 +9,9 @@ void check_balance();
 void withdraw_amount();
 void file_write_new_account_data(void);
 void default_menu(void);
-
+void update_account();
+void remove_account();
+void exisiting_account_details();
 
 const char *asterik = "***********************";
 const char *dash = "----------------------------------------------------------------------";
@@ -23,15 +25,13 @@ struct customer_data
     char customer_account_type[2];
 } c_data;
 
-
 struct amount
 {
     double deposit_amount;
     double withdraw_amount;
     double current_balance;
     double balance;
-}amnt;
-
+} amnt;
 
 int main(void)
 {
@@ -47,7 +47,7 @@ int main(void)
 }
 void menu_display()
 {
-     char temp;
+    char temp;
     int choice;
     printf("%s\n", dash);
     printf("%s Bank Management System %s \n", asterik, asterik);
@@ -56,7 +56,11 @@ void menu_display()
     printf("2. Deposit amount\n");
     printf("3. Withdraw amount\n");
     printf("4. Check balance\n");
-    printf("5. Exit");
+    printf("5. Update existing account\n");
+    printf("6. Details of existing account\n");
+    printf("7. Remove account\n");
+    printf("8. Exit");
+
     printf("\n%s\n", dash);
     printf("Enter your choice : ");
     scanf("%d", &choice);
@@ -82,6 +86,12 @@ void choice_select(int choice)
         check_balance();
         break;
     case 5:
+        update_account();
+    case 6:
+        exisiting_account_details();
+    case 7:
+        remove_account();
+    case 8:
         exit(0);
 
     default:
@@ -100,50 +110,78 @@ void create_new_account()
     fgets(c_data.customer_address, sizeof(c_data.customer_address), stdin);
     printf("\nChoose the account type (SA/CR): ");
     fgets(c_data.customer_account_type, sizeof(c_data.customer_account_type), stdin);
- getchar();
+    getchar();
     file_write_new_account_data();
- 
 }
 void file_write_new_account_data(void)
 {
     fprintf(customer_file_pt, "\nName: %s", c_data.customer_name);
     fprintf(customer_file_pt, "Date of Birth: %s", c_data.customer_dob);
-    fprintf(customer_file_pt, "\nPermanet address: %s", c_data.customer_address);
+    fprintf(customer_file_pt, "\nPermanent address: %s", c_data.customer_address);
     fprintf(customer_file_pt, "Account type: %s", c_data.customer_account_type);
+    printf("Your account has been created successfully!");
+    default_menu();
 }
 
 void deposit_amount(void)
 {
     amnt.deposit_amount = 0;
     printf("\nEnter the amount to be deposited: ");
-    scanf("%lf",&amnt.deposit_amount);
-    printf("%.2lf has been successfully deposited to your account",amnt.deposit_amount);
+    scanf("%lf", &amnt.deposit_amount);
+    printf("%.2lf has been successfully deposited to your account", amnt.deposit_amount);
     amnt.current_balance += amnt.deposit_amount;
     default_menu();
 }
 
-void withdraw_amount()
+void withdraw_amount(void)
 {
-    printf("Withdraw\n");
+    printf("\nEnter the amount to be withdrawn: ");
+    scanf("%lf", &amnt.withdraw_amount);
+    if (amnt.withdraw_amount > amnt.current_balance)
+    {
+        printf("\nInsufficient amount, please try again with lower deduction.");
+        default_menu();
+    }
+    else
+    {
+        amnt.current_balance -= amnt.withdraw_amount;
+        printf("%.2lf has been successfully withdrawn from your account", amnt.withdraw_amount);
+        default_menu();
+    }
 }
 
 void check_balance()
 {
-    printf("\n%.2f Balance in your account: ",amnt.current_balance);
+    printf("\nCurrent balance in your account: %.2f", amnt.current_balance);
     default_menu();
 }
 
 void default_menu(void)
 {
     int input_check;
- printf("\nPress 1 to go back to the main menu.Press 0 to terminate : ");
-        scanf("%d", &input_check);
-        if (input_check)
-        {
-            menu_display();
-        }
-        else
-        {
-            exit(0);
-        }
+    printf("\nPress 1 to go back to the main menu. Press 0 to terminate : ");
+    scanf("%d", &input_check);
+    if (input_check == 1)
+    {
+        menu_display();
+    }
+    else if (input_check == 0)
+    {
+        exit(0);
+    }
+    else
+    {
+        printf("Wrong input. Please try again");
+        default_menu();
+    }
+}
+
+void update_account()
+{
+}
+void remove_account()
+{
+}
+void exisiting_account_details()
+{
 }
