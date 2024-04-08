e31a47092f21abb1c91e7a583ac6bf1a4fe55c44
Vysakh <vysakhcs7@gmail.com>
2024-04-08 14:41:02 +0530

    changed printf statements

--- Structure/main.c
+++ Structure/main.c
@@ -18,19 +18,20 @@ int main(void)
 
     printf("Printing Address:\n");
     printf("s1 - %p\n", &s1);
+    printf("ptr - %p\n",&ptr);
     printf("s1.rollNo - %p\n", &s1.rollNo);
     printf("s1.name - %p\n", &s1.name);
     printf("s1.marks - %p\n", &s1.marks);
-    printf("Printing Values: \n");
+    printf("\nPrinting Values: \n");
     printf("s1 - %d\n", s1);
     printf("s1.rollNo - %d\n", s1.rollNo);
     printf("s1.name - %s\n", s1.name);
     printf("s1.marks - %f\n", s1.marks);
-    printf("Printing Pointer: \n");
-     printf("ptr - %p\n",&ptr);
+    printf("\nPrinting Pointer: \n");
+    printf("Address of ptr - %p\n",&ptr);
     printf("Value at address ptr - %p\n",ptr);
     printf("Dereferencing ptr - %d\n",*ptr);
-     printf("Dereferencing ptr to roll no - %d\n",(*ptr).rollNo);
-      printf("Dereferencing ptr to name - %s\n",ptr->name);
+    printf("Dereferencing ptr to roll no - %d\n",(*ptr).rollNo);
+    printf("Dereferencing ptr to roll no - %d\n",ptr->rollNo);
     return 0;
 }
